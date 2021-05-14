a0753c38fac3740c482e204f022a613c53b5b6a4
pra012 <83650177+pra012@users.noreply.github.com>
2021-05-14 11:01:36 +0530

    Create Exchangepositions.java

---
+++ Exchangepositions.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+ 
+public class test1 {
+	public static void main(String[] args) {
+		
+		String[] arr1 = {"HAHA","ING","LONE","SASA","LALA","XIXI"};
+		 
+		System.out.println(Arrays.toString(arr1));
+		 
+		swap(arr1,1,4);
+		 
+		System.out.println(Arrays.toString(arr1));
+		
+		
+		Integer[] arr2 = {85,64,96,89,82,665};
+		System.out.println(Arrays.toString(arr2));
+		 
+		swap(arr2,3,5);
+		System.out.println(Arrays.toString(arr2));
+	}
+	 
+	public static<T> void swap(T[] arr, int a, int b) {
+		 
+		T temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
