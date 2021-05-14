c4e83f956439fa5cd59e7f868b50cf13f79988dd
pra012 <83650177+pra012@users.noreply.github.com>
2021-05-14 11:04:59 +0530

    Create Maximalelement.java

---
+++ Maximalelement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public final class Algorithm {
+  
+    public static <T extends Object & Comparable<? super T>>
+        T max(List<? extends T> list, int begin, int end) {
+
+        T maxElem = list.get(begin);
+
+        for (++begin; begin < end; ++begin)
+            if (maxElem.compareTo(list.get(begin)) < 0)
+              
+                maxElem = list.get(begin);
+        return maxElem;
+    }
+}
